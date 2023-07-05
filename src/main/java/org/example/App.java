4d8a7a4cc7476762c47a6457debd8290e89c5430
Juan Oliver <juan_oliver@live.se>
2023-07-05 20:51:32 +0200

    "Updated version with option to en the program"

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,17 +7,17 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println
                 ("###################################");
 
         System.out.println
-        ("Hi, welcome to a simple calculator");
+                ("Hi, welcome to a simple calculator");
 
         System.out.println
                 ("############# Menu ################");
 
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Select an operation:");
         System.out.println("1. Addition");
@@ -25,11 +25,15 @@ public class App {
         System.out.println("3. Multiplication");
         System.out.println("4. Division");
 
+
         System.out.println
                 ("###################################");
 
         System.out.print("Enter your choice (1-4): ");
         int choice = scanner.nextInt();
+
+        System.out.println
+                ("###################################");
 
 
         System.out.print("Enter the first number: ");
@@ -39,6 +43,7 @@ public class App {
         double num2 = scanner.nextDouble();
 
         double result = 0.0;
+
 
         switch (choice) {
             case 1:
@@ -55,13 +60,93 @@ public class App {
                 break;
             default:
                 System.out.println("Invalid choice!");
+                scanner.close();
                 return;
         }
 
         System.out.println
                 ("###################################");
 
-        System.out.println("Result: " + result);
+        System.out.println("Result: "+result);
+
+        System.out.println
+                ("###################################");
+
+        boolean shouldContinue = true;
+
+        while (shouldContinue) {
+            System.out.println("\nDo you want to perform another calculation?");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+
+            System.out.println
+                    ("###################################");
+
+            System.out.print("Enter your choice (1-2): ");
+            int continueChoice = scanner.nextInt();
+
+            System.out.println
+                    ("###################################");
+
+            switch (continueChoice) {
+                case 1:
+                    System.out.println("Select an operation:");
+                    System.out.println("1. Addition");
+                    System.out.println("2. Subtraction");
+                    System.out.println("3. Multiplication");
+                    System.out.println("4. Division");
+
+                    System.out.println
+                            ("###################################");
+
+                    System.out.print("Enter your choice (1-4): ");
+                    choice = scanner.nextInt();
+
+                    System.out.print("\nEnter the first number: ");
+                    num1 = scanner.nextDouble();
+
+                    System.out.print("Enter the second number: ");
+                    num2 = scanner.nextDouble();
+
+
+                    result = 0.0;
+
+                    switch (choice) {
+                        case 1:
+                            result = num1 + num2;
+                            break;
+                        case 2:
+                            result = num1 - num2;
+                            break;
+                        case 3:
+                            result = num1 * num2;
+                            break;
+                        case 4:
+                            result = num1 / num2;
+                            break;
+                        default:
+                            System.out.println("Invalid choice!");
+                            scanner.close();
+                            return;
+                    }
+
+                    System.out.println("Result: " + result);
+                    break;
+                case 2:
+                    shouldContinue = false;
+                    System.out.println("Exiting calculator...");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Exiting calculator...");
+                    shouldContinue = false;
+                    break;
+            }
+        }
+
+
+
+
+
 
         System.out.println
                 ("###################################");
@@ -75,7 +160,5 @@ public class App {
         scanner.close();
 
 
-
-
-    }
+}
 }
